f0d266bec9b11c90e20d46a28531672b4e97afc2
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2025-09-07 16:41:24 +0100

    working

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*6x+fcy-=_jwmzhkn+ais%mgwch-!2iaw@b9fug!()7al7dl!%'
+SECRET_KEY = 'django-insecure-yacur!p%psu()^54tgjhy4s%ufbpqwj$wony%#g6mhwcr_wc1l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
+    'relationship_app',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
