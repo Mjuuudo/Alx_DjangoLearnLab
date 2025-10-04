d5a736ab70ad8e36010781fe568388df8f3fb70d
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2025-10-04 22:45:02 +0100

    Problem of Urls solved

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', registerView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('accounts/profile/', profilView.as_view(), name='profile'),
+    path('profile/', profilView.as_view(), name='profile'),
     path('logout/', logoutView, name='logout')
 ]
